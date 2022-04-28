323133efa8963acf0cf287fdab316d7020027c42
Blake <blakedeveloper@outlook.com>
2022-04-28 11:17:04 -0600

    Primer commit: Lista de Ejercicios agregados para python

---
+++ lista_de_ejercicios.py
@@ -0,0 +1,19 @@
+# Ejercicio 1 - Multiplicar dos numeros sin usar el simbolo de multiplicar
+
+# Ejercicio 2 - Ingresar nombre y apellido e imprimirlo al reves
+
+# Ejercicio 3 - Escibir una funcion que encuentre el elemento menor de una lista
+
+# Ejercicio 4 - Escribir una funcion que devuelve el volumen de una esfera por su radio
+# 4/3 * pi * r ** 3
+
+# Ejercicio 5 - Escribir una funcion que indique si el usuario es mayor de edad
+
+# Ejercicio 6 - Escribir una funcion que indique  si un numero es par o impar
+
+# Ejercicio 7 - Escribir una funcion que indique cuantas vocales tiene una palabra
+
+# Ejercicio 8 - Escribir una aplicacion que recibe una cantidad infinita de numeros hasta
+# decir basta, luego que devuelva la suma de los numeros ingresados
+
+# Ejercicio 9 - Escribir una funcion que recibe nombre y apellido y los vaya agregando a un archivo
